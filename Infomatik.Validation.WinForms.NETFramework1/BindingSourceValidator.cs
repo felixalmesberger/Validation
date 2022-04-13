4820fb3b9b99a366b07f77c3ab6936111bc11d73
Felix Almesberger <felix@almesberger.com>
2022-04-13 08:06:10 +0200

    feature: add .NET Framework versions

---
+++ Infomatik.Validation.WinForms.NETFramework1/BindingSourceValidator.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace Infomatik.Validation.WinForms;
+
+/// <summary>
+/// A component that validates Bindings on a given control.
+/// For this the DataSource if the Binding must implement the
+/// IValidatableObject interface. Errors will be visualized using
+/// an error provider
+/// </summary>
+[ToolboxBitmap(typeof(ErrorProvider))]
+public class BindingSourceValidator : Component, ISupportInitialize
+{
+  #region events
+
+  public event EventHandler? IsValidChanged;
+  private void OnIsValidChanged()
+  {
+    this.IsValidChanged?.Invoke(this, EventArgs.Empty);
+  }
+
+  #endregion
+
+  #region fields
+
+  private readonly IContainer? container;
+  private bool isValid;
+  private readonly ThrottledUiAction throttledValidation;
+  private BindingSource? bindingSource;
+
+  #endregion
+
+  #region constructors
+
+  public BindingSourceValidator()
+  {
+    this.throttledValidation = new(this.ValidateBindings)
+    {
+      ThrottleTimeInMs = 300
+    };
+  }
+
+  public BindingSourceValidator(IContainer container)
+    : this()
+  {
+    this.container = container;
+    container?.Add(this);
+  }
+
+  #endregion
+
+  #region properties
+
+  /// <summary>
+  /// ValidationStatusProvider used to show control validation results
+  /// </summary>
+  [Category("BindingSourceValidator")]
+  [Description]
+  public ValidationStatusProvider? ValidationStatusProvider { get; set; }
+
+  /// <summary>
+  /// Used validator
+  /// </summary>
+  [Category("BindingSourceValidator")]
+  [Description("Used validator")]
+  public IObjectValidator Validator { get; set; } = ObjectValidator.Default;
+
+  /// <summary>
+  /// Gets the validation state of monitored object
+  /// </summary>
+  public bool IsValid
+  {
+    get => this.isValid;
+    private set { this.isValid = value; this.OnIsValidChanged(); }
+  }
+
+  /// <summary>
+  /// Gets or sets, whether the validation is suspended
+  /// </summary>
+  [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+  [Category("BindingSourceValidator")]
+  public bool SuspendValidation { get; set; }
+
+  [DefaultValue(300)]
+  [Category("BindingSourceValidator")]
+  public int ThrottleTimeInMs
+  {
+    get => this.throttledValidation.ThrottleTimeInMs;
+    set => this.throttledValidation.ThrottleTimeInMs = value;
+  }
+
+  /// <summary>
+  /// All bindings belonging to <see cref="BindingSource"/>
+  /// </summary>
+  private IEnumerable<Binding> Bindings => this.bindingSource?.CurrencyManager.Bindings.OfType<Binding>() ?? Enumerable.Empty<Binding>();
+
+
+  /// <summary>
+  /// Gets or sets the monitored binding source
+  /// </summary>
+  public BindingSource? BindingSource
+  {
+    get => this.bindingSource;
+    set
+    {
+      if (value == this.BindingSource)
+        return;
+
+      this.DetachFromBindingSource();
+      this.bindingSource = value;
+      this.AttachToBindingSource();
+    }
+  }
+
+  #endregion
+
+  protected virtual bool AttachToBindingSource()
+  {
+    if (this.BindingSource is null)
+      return false;
+
+    this.BindingSource.BindingComplete += this.BindingComplete;
+    this.BindingSource.DataSourceChanged += this.DataSourceChanged;
+
+    return true;
+  }
+
+  protected virtual bool DetachFromBindingSource()
+  {
+    if (this.BindingSource is null)
+      return false;
+
+    this.BindingSource.BindingComplete -= this.BindingComplete;
+    this.BindingSource.DataSourceChanged -= this.DataSourceChanged;
+
+    return true;
+  }
+  private void BindingComplete(object? sender, BindingCompleteEventArgs e)
+  {
+    var hasBindingError = e.Exception != null;
+    if (hasBindingError)
+    {
+      this.ShowBindingError(e);
+    }
+    else
+    {
+      this.throttledValidation.Invoke();
+    }
+  }
+
+  private void DataSourceChanged(object? sender, EventArgs e)
+  {
+    this.throttledValidation.Invoke();
+  }
+
+  private void ShowBindingError(BindingCompleteEventArgs e)
+  {
+    if (e.Binding?.Control is null)
+      return;
+
+    this.ValidationStatusProvider?.SetErrorMessage(e.Binding.Control, e.ErrorText);
+  }
+
+  private void ValidateBindings()
+  {
+    if (this.SuspendValidation)
+      return;
+
+    if (this.BindingSource?.DataSource is null)
+      return;
+
+    if (this.ValidationStatusProvider is null)
+      return;
+
+    var validationResult = this.Validator.Validate(this.BindingSource.DataSource, breakOnFirstError: false);
+
+    foreach (var binding in this.Bindings)
+    {
+      if (binding.Control is null)
+        continue;
+
+      var boundProperty = binding.BindingMemberInfo.BindingMember;
+
+      var (error, warning, isRequired) = validationResult.GetPropertyError(boundProperty);
+
+      // if already showing required error, forget the others
+      if (!isRequired || string.IsNullOrEmpty(error))
+        this.ValidationStatusProvider.SetErrorMessage(binding.Control, error);
+
+      this.ValidationStatusProvider.SetWarnMessage(binding.Control, warning);
+      this.ValidationStatusProvider.SetIsMissingValue(binding.Control, isRequired);
+    }
+
+    this.IsValid = validationResult.IsValid;
+  }
+
+  private bool TryFindControlStatusProvider(out ValidationStatusProvider? errorProvider)
+  {
+    errorProvider = this.container?.Components.OfType<ValidationStatusProvider>().FirstOrDefault();
+    return errorProvider is not null;
+  }
+
+  public void BeginInit()
+  {
+  }
+
+  public void EndInit()
+  {
+    if (this.TryFindControlStatusProvider(out var errorProvider) && this.ValidationStatusProvider is null)
+      this.ValidationStatusProvider = errorProvider;
+  }
+
+  /// <summary>
+  /// Manually fires a validation
+  /// </summary>
+  public void Validate()
+  {
+    this.ValidateBindings();
+  }
+}
---
+++ Infomatik.Validation.WinForms.NETFramework1/ValidationStatusProvider.cs
@@ -0,0 +1,253 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Infomatik.Validation.WinForms.i18n;
+
+namespace Infomatik.Validation.WinForms;
+
+/// <summary>
+/// Capable of showing an error / warn icon or setting
+/// the cue banner for required errors
+/// </summary>
+[ToolboxBitmap(typeof(ErrorProvider))]
+[ProvideProperty("IconPadding", typeof(Control))]
+[ProvideProperty("IconAlignment", typeof(Control))]
+[ProvideProperty("IsMissingValue", typeof(Control))]
+[ProvideProperty("ErrorMessage", typeof(Control))]
+[ProvideProperty("WarnMessage", typeof(Control))]
+public class ValidationStatusProvider : Component, IExtenderProvider
+{
+  #region fields
+
+  private readonly ErrorProvider errorProvider = new()
+  {
+    BlinkStyle = ErrorBlinkStyle.NeverBlink
+  };
+
+  private readonly ErrorProvider warnProvider = new()
+  {
+    BlinkStyle = ErrorBlinkStyle.NeverBlink
+  };
+
+  private readonly IsMissingStatusProvider isMissingStatusProvider = new();
+
+  #endregion
+
+  #region constructors
+
+  public ValidationStatusProvider()
+  {
+    this.warnProvider.Icon = DefaultIcons.Warning;
+    this.errorProvider.Icon = DefaultIcons.Error;
+  }
+
+  public ValidationStatusProvider(IContainer? container)
+    : this()
+  {
+    container?.Add(this);
+  }
+
+  #endregion
+
+  #region properties
+
+  public string RequiredFallbackErrorMessage => Strings.Required;
+
+  #endregion
+
+  #region properties
+
+  /// <summary>
+  /// Gets or sets the Icon that displayed next to a control when a warn has been set for the control.
+  /// </summary>
+  [Category("ValidationStatusProvider")]
+  [Description("Gets or sets the Icon that displayed next to a control when a warn has been set for the control.")]
+  public Icon? WarnIcon
+  {
+    get => this.warnProvider.Icon;
+    set
+    {
+      if (value is null)
+        return;
+
+      this.warnProvider.Icon = value;
+    }
+  }
+
+
+  /// <summary>
+  /// Gets or sets the Icon that displayed next to a control when an error has been set for the control.
+  /// </summary>
+  [Category("ValidationStatusProvider")]
+  [Description("Gets or sets the Icon that displayed next to a control when an error has been set for the control.")]
+  public Icon? ErrorIcon
+  {
+    get => this.errorProvider.Icon;
+    set
+    {
+      if (value is null)
+        return;
+
+      this.errorProvider.Icon = value;
+    }
+  }
+
+  #endregion
+
+  #region provided properties
+
+  public void SetIconPadding(Control control, int padding)
+  {
+    this.errorProvider.SetIconPadding(control, padding);
+    this.warnProvider.SetIconPadding(control, padding);
+  }
+
+  public int GetIconPadding(Control control)
+  {
+    return this.errorProvider.GetIconPadding(control);
+  }
+
+  public void SetIconAlignment(Control control, ErrorIconAlignment alignment)
+  {
+    this.errorProvider.SetIconAlignment(control, alignment);
+    this.warnProvider.SetIconAlignment(control, alignment);
+  }
+
+  public ErrorIconAlignment GetIconAlignment(Control control)
+  {
+    return this.errorProvider.GetIconAlignment(control);
+  }
+
+  public void Clear(Control control)
+  {
+    this.SetIsMissingValue(control, false);
+    this.SetErrorMessage(control, null);
+  }
+
+  #endregion
+
+  public void SetIsMissingValue(Control control, bool isMissing)
+  {
+    // ReSharper disable once SuspiciousTypeConversion.Global
+    if (control is IVisualizeStatus visualizer)
+    {
+      visualizer.SetIsMissingValue(isMissing);
+      return;
+    }
+
+    this.InitializeDefaultControlIconAlignment(control);
+
+    var canUseRequiredProvider = this.isMissingStatusProvider.CanExtend(control);
+    var errorProviderShowingRequiredMessage = this.errorProvider.GetError(control) == RequiredFallbackErrorMessage;
+
+    if (!isMissing && errorProviderShowingRequiredMessage)
+      this.errorProvider.SetError(control, null);
+    else if (!isMissing && canUseRequiredProvider)
+      this.isMissingStatusProvider.SetShowRequiredMessage(control, false);
+    else if (isMissing && canUseRequiredProvider)
+      this.isMissingStatusProvider.SetShowRequiredMessage(control, true);
+    else if (isMissing && !canUseRequiredProvider)
+      this.errorProvider.SetError(control, RequiredFallbackErrorMessage);
+  }
+
+  public void SetErrorMessage(Control control, string? message)
+  {
+    // ReSharper disable once SuspiciousTypeConversion.Global
+    if (control is IVisualizeStatus visualizer)
+    {
+      visualizer.SetErrorMessage(message);
+      return;
+    }
+
+    this.InitializeDefaultControlIconAlignment(control);
+
+    if (message is not null)
+      this.warnProvider.SetError(control, null);
+
+    this.errorProvider.SetError(control, message);
+  }
+
+  public void SetWarnMessage(Control control, string? message)
+  {
+    // ReSharper disable once SuspiciousTypeConversion.Global
+    if (control is IVisualizeStatus visualizer)
+    {
+      visualizer.SetWarnMessage(message);
+      return;
+    }
+
+    this.InitializeDefaultControlIconAlignment(control);
+
+    if (message is not null)
+      this.errorProvider.SetError(control, null);
+    this.warnProvider.SetError(control, message);
+  }
+
+  /// <summary>
+  /// Visualize a warn message
+  /// </summary>
+  [Category("ValidationStatusProvider")]
+  [Description("Visualize a warn message")]
+  public string? GetWarnMessage(Control control)
+  {
+    // ReSharper disable once SuspiciousTypeConversion.Global
+    if (control is IVisualizeStatus visualizer)
+      return visualizer.GetWarnMessage();
+
+    return this.warnProvider.GetError(control);
+  }
+
+
+  /// <summary>
+  /// Visualize, that this control is bound to a required property
+  /// </summary>
+  [Category("ValidationStatusProvider")]
+  [Description("Visualize that this is a required field")]
+  public bool GetIsMissingValue(Control control)
+  {
+    // ReSharper disable once SuspiciousTypeConversion.Global
+    if (control is IVisualizeStatus visualizer)
+      return visualizer.GetRequiredError();
+
+    return this.isMissingStatusProvider.GetShowRequiredMessage(control);
+  }
+
+  /// <summary>
+  /// Visualize an error message
+  /// </summary>
+  [Category("ValidationStatusProvider")]
+  [Description("Visualize an error message")]
+  public string? GetErrorMessage(Control control)
+  {
+    // ReSharper disable once SuspiciousTypeConversion.Global
+    if (control is IVisualizeStatus visualizer)
+      return visualizer.GetErrorMessage();
+
+    return this.errorProvider.GetError(control);
+  }
+
+  /// <summary>
+  /// Is the control supported by the ValidationStatusProvider
+  /// </summary>
+  public bool CanExtend(object extendee)
+  {
+    return extendee is Control;
+  }
+
+  /// <summary>
+  /// Sets the default icon alignment to middle right with padding 5
+  /// </summary>
+  /// <param name="control"></param>
+  private void InitializeDefaultControlIconAlignment(Control control)
+  {
+    // if padding != 0, padding already set so return
+    if (this.errorProvider.GetIconPadding(control) != 0)
+      return;
+
+    this.errorProvider.SetIconAlignment(control, ErrorIconAlignment.MiddleRight);
+    this.errorProvider.SetIconPadding(control, 5);
+
+    this.warnProvider.SetIconAlignment(control, ErrorIconAlignment.MiddleRight);
+    this.warnProvider.SetIconPadding(control, 5);
+  }
+}
